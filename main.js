c17c5048b6ddc9b1d7c34a3b1ba8c4cedcb0e2cd
yaelz <Yael_Zaritsky@Yaels-MacBook-Pro-2.local>
2014-07-29 12:52:13 +0300

    Close button event working

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 "use strict";
-function GL() {
-
-}
+function GL() {}
 
 GL.createDiv = function createDiv(className) {
 //    className = className || "gl-item-title";
@@ -10,11 +8,12 @@ GL.createDiv = function createDiv(className) {
     return div;
 };
 
-GL.createBtn = function createBtn(text, onClickMethod) {
-    var showImgBtn = document.createElement('button');
-    showImgBtn.onclick = onClickMethod;
-    showImgBtn.textContent = text;
-    return showImgBtn;
+GL.createBtn = function createBtn(text, dataID, onClickMethod) {
+    var btn = document.createElement('button');
+    btn.onclick = onClickMethod;
+    btn.textContent = text;
+    btn.dataset.id = dataID;
+    return btn;
 };
 
 function createImg(imgSrc) {
@@ -24,12 +23,20 @@ function createImg(imgSrc) {
     return itemImg;
 }
 
-GL.removeItem = function removeItem(className) {
-    className.visibility = 'hidden';
+GL.hideItem = function hideItem(element) {
+    element.className = element.className + ' hidden';
 };
 
-GL.createItem = function createItem (imgSrc) {
+GL.removeItem = function removeItem(event) {
+    var button = event.target;
+    var elementID = button.dataset.id;
+    var element = document.getElementById(elementID);
+    element.parentNode.removeChild(element);
+};
+
+GL.createItem = function createItem (imgSrc, itemId) {
     var galleryItem = GL.createDiv("gl-gallery-item");
+    galleryItem.id = itemId;
 
     var itemTitle = GL.createDiv("gl-item-title");
     itemTitle.textContent = "Item Title";
@@ -41,12 +48,10 @@ GL.createItem = function createItem (imgSrc) {
     var itemControls = GL.createDiv("gl-item-controls");
     galleryItem.appendChild(itemControls);
 
-    var showImgBtn = GL.createBtn('Show image', function () {});
+    var showImgBtn = GL.createBtn('Show image', itemId, function () {});
     itemControls.appendChild(showImgBtn);
 
-//    var closeBtn = GL.createBtn('Close', GL.removeItem("gl-gallery-item"));
-//    itemControls.appendChild(closeBtn);
-    var closeBtn = GL.createBtn('Close', function () {});
+    var closeBtn = GL.createBtn('Close', itemId, GL.removeItem);
     itemControls.appendChild(closeBtn);
 
     return galleryItem;
@@ -55,9 +60,13 @@ GL.createItem = function createItem (imgSrc) {
 
 GL.init = function init() {
     var cont = document.querySelector('.gl-items-container');
-    cont.appendChild(GL.createItem('http://dummyimage.com/200x142/000/00ff48'));
-    cont.appendChild(GL.createItem('http://dummyimage.com/200x142/000/00ff48'));
-    cont.appendChild(GL.createItem('http://dummyimage.com/200x142/000/00ff48'));
+    [1,2,3,4].forEach(function (item) {
+        console.log(item);
+        cont.appendChild(GL.createItem('http://dummyimage.com/200x142/000/00ff48', item));
+    });
+//    cont.appendChild(GL.createItem('http://dummyimage.com/200x142/000/00ff48'));
+//    cont.appendChild(GL.createItem('http://dummyimage.com/200x142/000/00ff48'));
+//    cont.appendChild(GL.createItem('http://dummyimage.com/200x142/000/00ff48'));
 };
 
 GL.init();
