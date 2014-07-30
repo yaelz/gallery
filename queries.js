b2358570d2bcc9cd02a5be4a7048e252b493b350
yaelz <Yael_Zaritsky@Yaels-MacBook-Pro-2.local>
2014-07-30 11:02:15 +0300

    up till hasAttr

--- queries.js
+++ queries.js
@@ -6,20 +6,84 @@
     jDomItem.prototype.addClass = function (className) {
         this.element.classList.add(className);
         return this;
-    }
+    };
+
 
     jDomItem.prototype.removeClass = function (className) {
         this.element.classList.remove(className);
         return this;
-    }
+    };
 
-    //////////////////////////////////////////////////////////////////////
+    jDomItem.prototype.hasClass = function (className) {
+        return this.element.className.match(className) !== null;
+    };
+
+    jDomItem.prototype.hasAttr = function (attr) {
+        return this.element.hasAttribute(attr);
+    };
+
+    jDomItem.prototype.addAttr = function (key, val) {
+        this.element.setAttribute(key, val);
+        return this;
+    };
+
+    jDomItem.prototype.removeAttr = function (key) {
+        this.element.removeAttribute(key);
+        return this;
+    };
+
+
+    /////////////////////////// Collection ///////////////////////////////////////////
 
     function jDOMCollection(jDomItems) {
         this.nodes = jDomItems;
     }
 
-    jDOMCollection.prototype.each = function () {};
+
+    jDOMCollection.prototype.each = function (func) {
+        this.nodes.forEach(function(node) {
+            func.call(node);
+        });
+        return this;
+    };
+
+    jDOMCollection.prototype.hasClass = function (className) {;
+        return this.nodes[0].hasClass(className);
+    };
+
+    jDOMCollection.prototype.toggleClass = function (className) {;
+        this.each(function () {
+            if (!this.hasClass(className)) {
+                this.addClass(className);
+            } else {
+                this.removeClass(className);
+            }
+        });
+        return this;
+    };
+
+//    jDOMCollection.prototype.hasAttr = function hasAttr(attr) {
+//        return this.nodes[0].hasAttr(attr);
+//    };
+
+    jDOMCollection.prototype.addAttr = function addAttr(key, val) {;
+        this.each(function () {
+            this.addAttr(key, val);
+        });
+        return this;
+    };
+
+    jDOMCollection.prototype.removeAttr = function removeAttr(key) {;
+        this.each(function () {
+            this.removeAttr(key);
+        });
+        return this;
+    };
+
+    jDOMCollection.prototype.hasAttr = function (attr) {;
+        return this.nodes[0].hasAttr(attr);
+    };
+
     jDOMCollection.prototype.map = function () {};
     jDOMCollection.prototype.filter = function () {};
 
@@ -41,11 +105,11 @@
 
     $.is_jDomEntity = function(thing){
         return thing instanceof jDomItem || thing instanceof jDOMCollection;
-    }
+    };
 
     $.nodeListToArrayOfjDomItems = function(nodeList){
         return Array.prototype.map.call(nodeList, function (element) {
             return new jDomItem(element);
         });
-    }
+    };
 
--- spec/QueryTest.js
+++ spec/QueryTest.js
@@ -1,27 +1,83 @@
+"use strict";
 
 function setup(){
     document.body.innerHTML = '<div>ABC</div>';
 }
 
-setup();
+//setup();
+var div;
 
 describe("Queries Tests", function() {
-    describe("addClass", function() {
-        it("should return true", function() {
+
+    beforeEach(function() {
+        setup();
+        div = $('div');
+    });
+
+    describe("addClass function", function() {
+        it("should return true for instanceof jDOMCollection", function() {
             expect($('body') instanceof jDOMCollection).toEqual(true);
         });
 
-        it("should add class", function() {
-            $('div').nodes[0].addClass('hello');
-            var className = $('div').nodes[0].element.className;
+        it("should add class to jDOM item", function() {
+            div.nodes[0].addClass('hello');
+            var className = div.nodes[0].element.className;
             expect(className).toEqual('hello');
         });
 
-        it("should remove class", function() {
-            $('div').nodes[0].addClass('hello');
-            $('div').nodes[0].removeClass('hello');
-            var className = $('div').nodes[0].element.className;
+        it("should remove class from jDOM item", function() {
+            div.nodes[0].addClass('hello');
+            div.nodes[0].removeClass('hello');
+            var className = div.nodes[0].element.className;
             expect(className).toEqual('');
         });
     });
+
+    describe("functions on all elements", function() {
+
+        it("each function - should add class newclass for all elements", function() {
+            div.each(function () {
+                this.addClass('newclass');
+            });
+            expect(div.nodes[0].element.className).toEqual('newclass');
+        });
+
+        it("hasClass should return true", function() {
+            div.each(function () {
+                this.addClass('newclass');
+            });
+            expect(div.hasClass('newclass')).toEqual(true);
+        });
+
+        it("toggleClass should add class and then remove it", function() {
+            expect(div.hasClass('newclass')).toEqual(false);
+            div.toggleClass('newclass');
+            expect(div.hasClass('newclass')).toEqual(true);
+            div.toggleClass('newclass');
+            expect(div.hasClass('newclass')).toEqual(false);
+        });
+
+        it("addAttr should add the attribute", function() {
+            div = $('div');
+            div.addAttr('data-name', 'my-div');
+            expect(div.nodes[0].element.getAttribute('data-name')).toEqual('my-div');
+        });
+
+        it("removeAttr should remove the attribute", function() {
+            div.addAttr('data-name', 'hello');
+            div.removeAttr('data-name');
+            expect(div.nodes[0].element.getAttribute('data-name')).toEqual(null);
+        });
+
+        it("hasAttr should return true after adding an attribute", function() {
+            div.addAttr('data-name', 'hi');
+            expect(div.hasAttr('data-name')).toEqual(true);
+            console.log(div.nodes[0].element.getAttribute('data-name'));
+            div.removeAttr('data-name');
+            console.log(div.nodes[0].element.getAttribute('data-name'));
+            expect(div.hasAttr('data-name')).toEqual(false);
+        });
+    });
+
+
 });
